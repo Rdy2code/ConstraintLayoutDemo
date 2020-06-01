4929d295be87c1b3980e0a28ff26cb3789a94849
Louis Wollenberger <lvwmailbox@yahoo.com>
2020-06-01 06:25:04 -0700

    Initial commit

---
+++ app/src/main/java/com/example/android/colormyviews/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.android.colormyviews
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var boxOneText: TextView
+    private lateinit var boxTwoText: TextView
+    private lateinit var boxThreeText: TextView
+    private lateinit var boxFourText: TextView
+    private lateinit var boxFiveText: TextView
+    private lateinit var constraintLayout: ConstraintLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setListeners()
+    }
+
+    private fun setListeners() {
+        boxOneText = findViewById(R.id.box_one_text)
+        boxTwoText = findViewById(R.id.box_two_text)
+        boxThreeText = findViewById(R.id.box_three_text)
+        boxFourText = findViewById(R.id.box_four_text)
+        boxFiveText = findViewById(R.id.box_five_text)
+        constraintLayout = findViewById(R.id.constrain_layout)
+
+        val clickableViews: List<View> =
+            listOf(boxOneText, boxTwoText, boxThreeText, boxFourText, boxFiveText, constraintLayout)
+
+        for (item in clickableViews) {
+            item.setOnClickListener {makeColored(it)}
+        }
+    }
+
+    private fun makeColored (view: View) {
+        when (view.id) {
+            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+            R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
+            R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
+            R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
+            else -> view.setBackgroundColor(Color.LTGRAY)
+        }
+    }
+}
